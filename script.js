c0c3a0d9438843df5052e2bfc7466123ab65e9d0
mungaig <geoffiscoding@gmail.com>
2024-12-03 10:42:36 +0300

    Add play round and loop functionality

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
 let humanScore = 0;
 let computerScore = 0;
+
 //Getting the computer's choice
 function getComputerChoice(){
     const choiceList = ['rock', 'paper', 'scissors'];
     //Math.random() works great here because arrays are indexed from 0
     const computerChoice = choiceList[Math.floor(Math.random() * choiceList.length)];
-    console.log(computerChoice);
+    return computerChoice;
 }
 
 function getHumanChoice(){
     let playerChoice = prompt('rock, paper, scissors').toLowerCase();
-    console.log(playerChoice);
+    return playerChoice;
 }
 
+function playRound(humanChoice, computerChoice){
+    console.log('You: ' + humanChoice);
+    console.log('Computer: ' + computerChoice);
+    if(humanChoice === computerChoice){
+        console.log('Draw! You both choose ' + humanChoice);
+    } else if(humanChoice === 'rock' && computerChoice === 'paper'){
+        console.log('You lose! paper beats rock');
+        computerScore += 1;
+    } else if(humanChoice === 'paper' && computerChoice === 'scissors'){
+        console.log('You lose! scissors beats paper');
+        computerScore += 1;
+    } else if(humanChoice === 'scissors' && computerChoice === 'rock'){
+        console.log('You lose! rock beats scissors')
+    } else if(humanChoice === 'paper' && computerChoice === 'rock'){
+        console.log('You win! paper beats rock');
+        humanScore += 1;
+    } else if(humanChoice === 'scissors' && computerChoice === 'paper'){
+        console.log('You win! scissors beats paper');
+        humanScore += 1;
+    } else if(humanChoice === 'rock' && computerChoice === 'scissors'){
+        console.log('You win! rock beats scissors');
+        humanScore += 1;
+    }
+ }
+
+ for(let i = 0; i < 5; i++){
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+ }
+
+ console.log(`Total scores:
+    You: ${humanScore}
+    computer: ${computerScore}`);
+
+if(humanScore === computerScore){
+    console.log('Game ends in a draw');
+} else if(humanScore > computerScore){
+    console.log('Congratulations! You win the game');
+} else {
+    console.log('Sorry! Computer wins the game')
+}
